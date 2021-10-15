cc8a487b1495b34f6514cb41dbfc5eecd96de4a2
hcs <hcs1835738861@163.com>
2021-10-16 00:07:17 +0800

    second commit Test.java

--- Test.java
+++ Test.java
@@ -1,5 +1,7 @@
 ﻿public class Test {
 	public static void main(String[] args) {
 		System.out.println("helllo world");
+		System.out.println("helllo world");
+		System.out.println("helllo world");
 	}
 }
